62092b26d4f831e8b8a57d0d5895343341209cc7
Enzo Turano <enzogturanoc@gmail.com>
2024-03-13 08:31:35 -0300

    Criado arquivo para conceitos de variaveis

---
+++ aula01/funcoes.js
@@ -0,0 +1,9 @@
+let num1 = 1
+const num2 = 3
+var msg = "Olá amigos! Este é o número: "
+
+console.log(msg + num1)
+console.log(msg , num1)
+console.log(num1 + num2)
+console.log(typeof num1)
+console.log(typeof msg)
