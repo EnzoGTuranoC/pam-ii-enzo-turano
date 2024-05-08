45c563d969042b0bf96960740b68dedc2193fced
Enzo Turano <enzogturanoc@gmail.com>
2024-05-08 08:27:10 -0300

    Aula 04

---
+++ aula04/funcoes2.js
@@ -0,0 +1,8 @@
+const TesteImparPar = (n) => {
+    let calculoTeste = n % 2
+
+    return (calculoTeste == 1) ? "Número ímpar" : "Número par"
+}
+console.log(TesteImparPar(17))
+console.log(TesteImparPar(64))
+console.log(TesteImparPar(1500))
